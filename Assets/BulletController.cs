1d826dc395f2de191c681f42d1c952dccdb15d98
suhyun09 <kier09@naver.com>
2022-05-09 15:02:57 +0900

    [총알 문제 해결]
    총알 문제 날라가는 것 해결
    UI 텍스트를 이용해 점수 체크하고 표시함

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -13,10 +13,10 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            Shoot(new Vector3(0,0,-20));
-        }
+        // if(Input.GetMouseButtonDown(0))
+        //{
+        //     Shoot(new Vector3(0,0,-20));
+        //}
     }
     public void Shoot (Vector3 dir)
     {
@@ -26,6 +26,8 @@ public class BulletController : MonoBehaviour
     {
         if(collision.collider.tag == "ENEMY")
         {
+            GameObject manager = GameObject.Find ("ScoreManager");
+            manager.GetComponent<ScoreManager>().IncScore();
             Destroy (gameObject, 0.2f);
         }
     }
